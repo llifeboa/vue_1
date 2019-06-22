185ad2f533e2347a5a91389a79fe23db71e0080a
Илья Шабров <ilionis@MacBook-Air-Ila-2.local>
2019-06-22 19:23:35 +0300

    test.json

---
+++ vue_1/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+ 	<table>
+	<tr v-for="user in users">
+		<th></th>
+	</tr>
+ 	</table>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+  	return({
+  		users: {}
+  		});
+  },
+  created: function(){
+  	this.users = fetch('https://gorest.co.in/public-api/users?_format=json&access-token=zbduG6SiEK7TekrUcCzWYrZPEKbWt49RcW-f')
+  	.then(response => response.json())
+  	.then(json => console.log(JSON.stringify(json)))
+  },
+  props: {
+	msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
